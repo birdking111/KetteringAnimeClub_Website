426d3fa5c1f166d7e61650cb3ef2db695fffdba2
birdking111 <birdking111@yahoo.com>
2015-06-08 17:05:15 -0400

    Styleshee~ts!!!
    Added some CSS files to the project. In such is a reset file and the
    start to the default css file.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 function getNavBar(){
+		
+		//Create an iterator helper for lists
 		Handlebars.registerHelper('list', function(context, options) {
           var ret = "";
 
@@ -8,12 +10,13 @@ function getNavBar(){
 
           return ret;
         });
-	  
+	    //recognize the source template
         var source   = $("#navigation-bar").html();
 	    //console.log("Source 1 "  +source);
 	    var template = Handlebars.compile(source);
 	  
-	    var context = {
+	    //set the template's context
+		var context = {
 	      nav: [
 		    {anchor: "<a href=''>", title: "Home"},
 		    {anchor: "<a href=''>", title: "About Us"},
@@ -24,12 +27,24 @@ function getNavBar(){
 		    {anchor: "<a class='dropdown-button' href='#!'>", title: "Events"},
 		  ]
 	    };
+		//create an insert variable
 		var el_html = template(context);
-		  
+				
 		$("#navigation").html(el_html);
 		
+		//quickly insert a placeholder for the submenu, hide it
+		//$("<div id='events'></div>").attr('hidden', true).insertAfter(".dropdown-button");
 		$("<div id='events'></div>").insertAfter(".dropdown-button");
 		
+		//insert a hover event
+		//$(".dropdown-button").hover(function(){
+		//	$("#events").removeAttr('hidden');
+		//},
+		//function(){
+		//	$("#events").attr('hidden',true);
+		//});
+		
+		//repeat steps for declaring submenu
 		source   = $("#navigation-subbar").html();
 		template = Handlebars.compile(source);
 		context  = {
@@ -48,8 +63,10 @@ function getNavBar(){
 
 $(document).ready(function(){
       
+	  //create navigation bar
 	  var navigation = getNavBar()
 	  
+	  //compile the body
 	  var source   = $("#entry-template").html();
 	  //console.log("Source 2 " + source);
       var template = Handlebars.compile(source);
