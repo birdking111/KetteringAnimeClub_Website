88eeaf23240186c14bd12e6175448d1140274ab8
birdking111 <birdking111@yahoo.com>
2015-06-16 13:19:33 -0400

    More Dynamic
    After fixing a mistake from last night. I managed to add dynamic
    templating and dynamic content population to the site.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,54 @@
+//get the current template from the #href
+function fetchTemplate() {
+  var hash = window.location.hash || '#home-page';
+  return hash.replace(/[/!]/g, '');
+};
+
+//created a list of hashes
+function hashContexts() {
+  //define the context map
+  var contextHash = {
+    //declare context
+    '#home-page': {title: "Welcome to the KU Anime Club", body: "Greetings fellow lover of Japanimation. Welcome to our club!"},
+    '#about-page': {},
+    '#rules-page' : {},
+    '#showings-page' : {},
+    '#recommend-page' : {},
+    '#collection-page' : {},
+    '#kucon-event' : {},
+    '#animefest-event' : {},
+    '#sushinight-event' : {},
+  };
+  
+  return contextHash;
+};
+
+function constructContent() {
+  //declare the url hash to a variable
+  var currentPage = fetchTemplate();
+  
+  //in case the current page is an event, change the source
+  if (currentPage.contains("-event")) var source = $("event-page").html();
+  else var source = $(fetchTemplate()).html();
+  
+  console.log("Source = " + source);
+  console.log("currentPage = " + currentPage);
+  
+  var template = Handlebars.compile(source);
+	  
+  //set the template's context from a hash
+  //with the same hashvariable used for source
+  var contextHash = hashContexts();
+  var context = contextHash[currentPage];
+  
+  console.log("Context = " + contextHash[currentPage]);
+  
+  //create an insert variable
+  var el_html = template(context);
+  //place into the context area
+  $("#content").html(el_html);
+};
+
 function createNav(){
 		
 		//Create an iterator helper for lists
@@ -18,12 +69,12 @@ function createNav(){
 	    //set the template's context
 		var context = {
 	      nav: [
-		    {anchor: "<a href=''>", title: "Home"},
-		    {anchor: "<a href=''>", title: "About Us"},
-		    {anchor: "<a href=''>", title: "Club Rules"},
-		    {anchor: "<a href=''>", title: "Showings"},
-		    {anchor: "<a href=''>", title: "Recommend Anime"},
-			{anchor: "<a href=''>", title: "Collection"},
+		    {anchor: "<a href='#home-page'>", title: "Home"},
+		    {anchor: "<a href='#about-page'>", title: "About Us"},
+		    {anchor: "<a href='#rules-page'>", title: "Club Rules"},
+		    {anchor: "<a href='#showings-page'>", title: "Showings"},
+		    {anchor: "<a href='#recommend-page'>", title: "Recommend Anime"},
+			{anchor: "<a href='#collection-page'>", title: "Collection"},
 		    {anchor: "<a class='dropdown-button' href='#!'>", title: "Events"},
 		  ]
 	    };
@@ -37,10 +88,10 @@ function createNav(){
 		template = Handlebars.compile(source);
 		context  = {
 			nav: [
-			  {anchor: "<a href=''>", title: "Events"},
-			  {anchor: "<a href=''>", title: "KU Con"},
-			  {anchor: "<a href=''>", title: "Anime Fest"},
-			  {anchor: "<a href=''>", title: "Sushi Night"},
+			  {anchor: "<a href='!#'>", title: "Events"},
+			  {anchor: "<a href='#kucon-event'>", title: "KU Con"},
+			  {anchor: "<a href='#animefest-event'>", title: "Anime Fest"},
+			  {anchor: "<a href='sushinight-event'>", title: "Sushi Night"},
 			]
 		}
 		
@@ -58,15 +109,5 @@ $(document).ready(function(){
 	  //create navigation bar
 	  var navigation = createNav()
 	  
-	  //compile the body
-	  var source   = $("#entry-template").html();
-	  //console.log("Source 2 " + source);
-      var template = Handlebars.compile(source);
-	  
-	  var context = {title: "My New Post", body: "This is my first post!"};
-      var el_html = template(context);
-	  
-	  $("#content").html(el_html);
-	  
-	  
+	  constructContent();
 });
